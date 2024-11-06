0405b7ab813c6bf9b73d5bd6ac53854972fb1c2b
nishiokozo <nishiokozo@gmail.com>
2024-11-06 21:24:30 +0900

    離心円、周転円の追加

--- graph-planets/main.js
+++ graph-planets/main.js
@@ -123,18 +123,16 @@ let html =
 		}
 	}
 };
-function strfloat(v, r = 4, f = 2) {
-  if (typeof v !== 'number') {
-    throw new Error('Input must be a number');
-  }
 
-  // 小数点以下 f 桁で固定小数点表記に変換
-  const fixed = v.toFixed(f);
+//-----------------------------------------------------------------------------
+function strfloat( v, r=4, f=2 ) // r指数部桁、f小数部桁
+//-----------------------------------------------------------------------------
+{
+	// 小数点以下 f 桁で固定小数点表記に変換
+	const fixed = v.toFixed(f);
 
-  // 必要に応じてスペースで埋める
-  const formatted = fixed.padStart(r + f + 1, ' '); // `+1` は小数点分
-
-  return formatted;
+	// 必要に応じてスペースで埋める
+	return fixed.padStart(r + f + 1, ' '); // `+1` は小数点分
 }
 //-----------------------------------------------------------------------------
 function gra_create( cv )	//2021/06/01 window関数実装
@@ -298,6 +296,8 @@ function main()
 //-----------------------------------------------------------------------------
 {
 	const Au = 1.496e+8;
+	let param_flgDeferent = false;
+	let param_flgEpicycle = false;
 	let param_speed = 1;
 	let param_size = 20;
 	let param_center = "Sun";
@@ -308,7 +308,7 @@ function main()
 		"Mer"	:{name:"水星"	,radius:2439	,semi_major_axis:Au*0.387	,orbital_period:0.241		,x:0,y:0,rad:0,visible:true},
 		"Ven"	:{name:"金星"	,radius:6051	,semi_major_axis:Au*0.723	,orbital_period:0.615		,x:0,y:0,rad:0,visible:true},
 		"Ear"	:{name:"地球"	,radius:6378	,semi_major_axis:Au*1.0000	,orbital_period:1.000		,x:0,y:0,rad:0,visible:true},
-		"Moo"	:{name:"月"		,radius:1737	,semi_major_axis:384748		,orbital_period:1/(29.5*12)	,x:0,y:0,rad:0,visible:false},
+		"Moo"	:{name:"月"		,radius:1737	,semi_major_axis:384748		,orbital_period:(29.5*12)/365	,x:0,y:0,rad:0,visible:false},
 		"Mar"	:{name:"火星"	,radius:3396	,semi_major_axis:Au*1.524	,orbital_period:1.881		,x:0,y:0,rad:0,visible:true},
 		"Jup"	:{name:"木星"	,radius:71492	,semi_major_axis:Au*5.204	,orbital_period:11.862		,x:0,y:0,rad:0,visible:true},
 		"Sat"	:{name:"土星"	,radius:60268	,semi_major_axis:Au*9.582	,orbital_period:29.457		,x:0,y:0,rad:0,visible:true},
@@ -317,18 +317,18 @@ function main()
 	}
 	let	treeStellar = 
 	[
-		{stellar:param_tblStellar["Sun"],child:
+		{body:param_tblStellar["Sun"],child:
 		[
-			{stellar:param_tblStellar["Mer"],child:[]},
-			{stellar:param_tblStellar["Ven"],child:[]},
-			{stellar:param_tblStellar["Ear"],child:[
-				{stellar:param_tblStellar["Moo"],child:[]},
+			{body:param_tblStellar["Mer"],child:[]},
+			{body:param_tblStellar["Ven"],child:[]},
+			{body:param_tblStellar["Ear"],child:[
+				{body:param_tblStellar["Moo"],child:[]},
 			]},
-			{stellar:param_tblStellar["Mar"],child:[]},
-			{stellar:param_tblStellar["Jup"],child:[]},
-			{stellar:param_tblStellar["Sat"],child:[]},
-			{stellar:param_tblStellar["Ura"],child:[]},
-			{stellar:param_tblStellar["Nep"],child:[]},
+			{body:param_tblStellar["Mar"],child:[]},
+			{body:param_tblStellar["Jup"],child:[]},
+			{body:param_tblStellar["Sat"],child:[]},
+			{body:param_tblStellar["Ura"],child:[]},
+			{body:param_tblStellar["Nep"],child:[]},
 		]}
 	];
 
@@ -336,6 +336,17 @@ function main()
 	let date = 1;
 	let cx = 0;
 	let cy = 0;
+	
+	//-------------------------------------------------------------------------
+	function treesearch( tree, param, func )
+	//-------------------------------------------------------------------------
+	{
+		for ( let i = 0 ; i < tree.length ; i++ )
+		{
+			let ret = func(tree[i].body,param);
+			if ( tree[i].child.length > 0 ) treesearch( tree[i].child, ret, func );
+		}
+	}
 	//-------------------------------------------------------------------------
 	function frame_update( time )
 	//-------------------------------------------------------------------------
@@ -346,27 +357,27 @@ function main()
 
 		date += step;
 
-		//-------------------------------------------------------------------------
-		function calcTree( ox,oy,tree )
-		//-------------------------------------------------------------------------
+		treesearch( treeStellar,null, function(s,p)
 		{
-			for ( let i = 0 ; i < tree.length ; i++ )
+			if ( s.orbital_period > 0 ) s.rad += (2*Math.PI/365) * (step/s.orbital_period);
+			s.x = s.semi_major_axis*Math.cos(s.rad);
+			s.y = s.semi_major_axis*Math.sin(s.rad);
+			if ( p != null )
 			{
-				let s = tree[i].stellar;
-				if ( s.orbital_period > 0 ) s.rad += (2*Math.PI/365) * (step/s.orbital_period);
-				s.x = s.semi_major_axis*Math.cos(s.rad)+ox;
-				s.y = s.semi_major_axis*Math.sin(s.rad)+oy;
-				if ( tree[i].child.length > 0 ) calcTree( s.x,s.y,tree[i].child );
+				s.x += p.x;
+				s.y += p.y;
 			}
-		}
-		calcTree( 0,0,treeStellar);
+			return s;
+		});
 
 		{
 			cx = param_tblStellar[param_center].x;
 			cy = param_tblStellar[param_center].y;
 		}
 
-		// draw
+		// draw 情報 
+		gra1.color("#000");
+		gra2.color("#000");gra2.font("16px");
 		gra2.cls();
 		{
 			{
@@ -383,53 +394,70 @@ function main()
 				}
 
 				let y = 0 ;
-				//-------------------------------------------------------------------------
-				function foo( tree )
-				//-------------------------------------------------------------------------
-				{
-					for ( let i = 0 ; i < tree.length ; i++ )
-					{
-						let s = tree[i].stellar;
-						let str = "公転周期="+strfloat(s.orbital_period,3,4)+"(年)";
-						gra2.print( s.name, 0,y );
-						gra2.print( str, 60,y );
-						y+=20;
 
-						if ( tree[i].child.length > 0 ) foo( tree[i].child );
-					}
-				}
-				foo( treeStellar );
+				treesearch( treeStellar,null, function(s,p)
+				{
+					let str = "公転周期="+strfloat(s.orbital_period,3,2)+"(年)";
+					gra2.print( s.name, 0,y );
+					gra2.print( str, 60,y );
+					y+=20;
+				});
 			}
+
+
+		}
+
+		// draw 天体 
+		{
+			let sz_y = Au*param_size;
+			let sz_x = Math.floor(html_canvas2.width/html_canvas2.height*sz_y);
+			gra1.window( -sz_x+cx, sz_y+cy, sz_x+cx, -sz_y+cy );
+			gra2.window( -sz_x+cx, sz_y+cy, sz_x+cx, -sz_y+cy );
+		}
+
+		treesearch( treeStellar, 0, function(s,p)
+		{
+			if ( s.visible == true )
+			{
+				gra1.dotty( s.x,s.y,1 );
+				gra2.circle( s.x,s.y,s.radius );
+				gra2.dotty( s.x,s.y,3 );
+				gra2.print( s.name, s.x,s.y );
+			}
+		});
+
+		// 周転円の描画
+		{
 			{
 				let sz_y = Au*param_size;
 				let sz_x = Math.floor(html_canvas2.width/html_canvas2.height*sz_y);
-				gra1.window( -sz_x+cx, sz_y+cy, sz_x+cx, -sz_y+cy );
-				gra2.window( -sz_x+cx, sz_y+cy, sz_x+cx, -sz_y+cy );
+				gra1.window( -sz_x, sz_y, sz_x, -sz_y );
+				gra2.window( -sz_x, sz_y, sz_x, -sz_y );
 			}
-		}
-
-		gra1.color("#000");
-		gra2.color("#000");gra2.font("16px");
-
-
-		//-------------------------------------------------------------------------
-		function drawTree( tree )
-		//-------------------------------------------------------------------------
-		{
-			for ( let i = 0 ; i < tree.length ; i++ )
+			gra2.color("#00F");
+			gra1.color("#F00");
+			treesearch( treeStellar, 0, function(s,p)
 			{
-				let s = tree[i].stellar;
 				if ( s.visible == true )
 				{
-					gra1.dotty( s.x,s.y,0.5 );
-					gra2.circle( s.x,s.y,s.radius );
-					gra2.dotty( s.x,s.y,3 );
-					gra2.print( s.name, s.x,s.y );
+					let c = param_tblStellar[param_center];
+					if ( c != s )
+					{
+						let min = s.semi_major_axis-c.semi_major_axis;
+						let max = s.semi_major_axis+c.semi_major_axis;
+						let epir=(max-min)/2;
+						let r = (max+min)/2;
+
+						let x = r*Math.cos(s.rad);
+						let y = r*Math.sin(s.rad);
+
+console.log(param_flgDeferent,param_flgEpicycle);
+						if ( param_flgDeferent ) gra1.dotty( x,y, 1 );
+						if ( param_flgEpicycle ) gra2.circle( x,y,epir );
+					}
 				}
-				if ( tree[i].child.length > 0 ) drawTree( tree[i].child );
-			}
+			});
 		}
-		drawTree( treeStellar);
 	}
 	
 	let prev = 0;
@@ -439,22 +467,24 @@ function main()
 	{
 		if ( g_req_stat != null )
 		{
-			param_tblStellar["Sun"].visible=g_req_stat["Sun"].value;
-			param_tblStellar["Mer"].visible=g_req_stat["Mer"].value;
-			param_tblStellar["Ven"].visible=g_req_stat["Ven"].value;
-			param_tblStellar["Ear"].visible=g_req_stat["Ear"].value;
-			param_tblStellar["Moo"].visible=g_req_stat["Moo"].value;
-			param_tblStellar["Mar"].visible=g_req_stat["Mar"].value;
-			param_tblStellar["Jup"].visible=g_req_stat["Jup"].value;
-			param_tblStellar["Sat"].visible=g_req_stat["Sat"].value;
-			param_tblStellar["Ura"].visible=g_req_stat["Ura"].value;
-			param_tblStellar["Nep"].visible=g_req_stat["Nep"].value;
-			param_center = g_req_stat["Center"].value+"";
-			param_size = g_req_stat["Size"].value;
-			param_speed = g_req_stat["Speed"].value;
+			param_tblStellar["Sun"].visible=g_req_stat["Sun"];
+			param_tblStellar["Mer"].visible=g_req_stat["Mer"];
+			param_tblStellar["Ven"].visible=g_req_stat["Ven"];
+			param_tblStellar["Ear"].visible=g_req_stat["Ear"];
+			param_tblStellar["Moo"].visible=g_req_stat["Moo"];
+			param_tblStellar["Mar"].visible=g_req_stat["Mar"];
+			param_tblStellar["Jup"].visible=g_req_stat["Jup"];
+			param_tblStellar["Sat"].visible=g_req_stat["Sat"];
+			param_tblStellar["Ura"].visible=g_req_stat["Ura"];
+			param_tblStellar["Nep"].visible=g_req_stat["Nep"];
+			param_center		= g_req_stat["Center"];
+			param_size			= g_req_stat["Size"];
+			param_speed			= g_req_stat["Speed"];
+			param_flgDeferent	= g_req_stat["Deferent"];
+			param_flgEpicycle	= g_req_stat["Epicycle"];
 
 			gra1.cls();
-			year=0
+			year=0;
 			g_req_stat = null;
 		}
 
@@ -477,30 +507,34 @@ function main()
 window.onload = function(e)
 //-----------------------------------------------------------------------------
 {
-	html_request();
+	html_request('');
 	main();
 }
 let g_req_requestAnimationFrame = null;
 let g_req_stat = null;
 //-----------------------------------------------------------------------------
-function html_request()
+function html_request( cmd )	// 正規化もここで行うと、入力エラーがあってもmainに被害が無いので都合がいい
 //-----------------------------------------------------------------------------
 {
 	let new_stat = 
 	{
-		"Sun"	:{value:html.getByName_checkbox("html_checkbox_Sun",false)},
-		"Mer"	:{value:html.getByName_checkbox("html_checkbox_Mer",false)},
-		"Ven"	:{value:html.getByName_checkbox("html_checkbox_Ven",false)},
-		"Ear"	:{value:html.getByName_checkbox("html_checkbox_Ear",false)},
-		"Moo"	:{value:html.getByName_checkbox("html_checkbox_Moo",false)},
-		"Mar"	:{value:html.getByName_checkbox("html_checkbox_Mar",false)},
-		"Jup"	:{value:html.getByName_checkbox("html_checkbox_Jup",false)},
-		"Sat"	:{value:html.getByName_checkbox("html_checkbox_Sat",false)},
-		"Ura"	:{value:html.getByName_checkbox("html_checkbox_Ura",false)},
-		"Nep"	:{value:html.getByName_checkbox("html_checkbox_Nep",false)},
-		"Center":{value:html.getByName_radio("html_radio","Sun")+""},
-		"Size"	:{value:html.getById_textbox("html_size","1")*1.0},
-		"Speed"	:{value:html.getById_textbox("html_speed","1")*1.0},
+		"Sun"		:html.getByName_checkbox("html_checkbox_Sun",false),
+		"Mer"		:html.getByName_checkbox("html_checkbox_Mer",false),
+		"Ven"		:html.getByName_checkbox("html_checkbox_Ven",false),
+		"Ear"		:html.getByName_checkbox("html_checkbox_Ear",false),
+		"Moo"		:html.getByName_checkbox("html_checkbox_Moo",false),
+		"Mar"		:html.getByName_checkbox("html_checkbox_Mar",false),
+		"Jup"		:html.getByName_checkbox("html_checkbox_Jup",false),
+		"Sat"		:html.getByName_checkbox("html_checkbox_Sat",false),
+		"Ura"		:html.getByName_checkbox("html_checkbox_Ura",false),
+		"Nep"		:html.getByName_checkbox("html_checkbox_Nep",false),
+		"Center"	:html.getByName_radio("html_radio","Sun")+"",
+		"Size"		:html.getById_textbox("html_size","1")*1.0,
+		"Speed"		:html.getById_textbox("html_speed","1")*1.0,
+		"Deferent"	:html.getByName_checkbox("html_checkbox_Deferent",false),
+		"Epicycle"	:html.getByName_checkbox("html_checkbox_Epicycle",false),
+		//
+		"cmd"		:cmd,
 	}
 
 	g_req_stat = new_stat;
